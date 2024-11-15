2a68c3ab75f5e87dd69b2cbb50b176dec4562a2d
Fran Aragon <franaragonmesa@gmail.com>
2024-11-15 16:04:46 +0100

    update

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,7 +4,7 @@ import styles from './About.module.scss';
 const About: React.FC = () => (
   <div className={styles.about}>
     <h1 className={styles.title}>About This App</h1>
-    <p>This is an example application built with CRA React.</p>
+    <p>This is an example application built with Vite React.</p>
   </div>
 );
 
